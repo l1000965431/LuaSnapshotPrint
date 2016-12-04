ed469feba4326023e399f79e8c5d099d8e6fa481
l1000965431 <l1000965431@126.com>
2016-12-04 23:12:40 +0800

    插件.cs

---
+++ LuaSnapshot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using LuaInterface;
+using LuaFramework;
+
+public class LuaSnapshot {
+
+    [MenuItem("LuaSnapshot/LuaSnapshotPrint")]
+    static void LuaSnapshotPrint()
+    {       
+        LuaManager mgr = AppFacade.Instance.GetManager<LuaManager>(ManagerName.Lua);
+        mgr.CallFunction("Game.dump");
+    }
+}
